6b61fb9c9de3a6d0f584f05b13dbea649f691938
edupoly <info.edupoly@gmail.com>
2024-12-19 18:37:17 +0530

    vmodel

--- src/Todolist.vue
+++ src/Todolist.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { reactive } from "vue";
+
+import { reactive, watch } from "vue";
 import Todo from './Todo.vue'
     var state = reactive({
         todos:[
@@ -15,21 +16,36 @@ import Todo from './Todo.vue'
                 title:'get tea',
                 status:true
             }
-        ]
+        ],
+        newtodo:'',
+        gender:'',
+        techs:[]
     })
     function add(){
-        state.todos.push({title:document.getElementById("d1").value,status:false})
+        state.todos.push({title:state.newtodo,status:false})
     }
     function toggle(i){
         state.todos[i].status=!state.todos[i].status;
     }
+    watch(state,()=>{console.log("Hello")})
 </script>
 <template>
-    <input type="text" id="d1">
+    <input v-model="state.newtodo">
     <button @click="add">Add Todo</button>
+    <br>
+    <input type="checkbox" v-model="state.techs" value='rjs'>:ReactJS
+    <input type="checkbox" v-model="state.techs" value='njs'>:NodeJS
+    <input type="checkbox" v-model="state.techs" value='vjs'>:VueJS
+    <input type="checkbox" v-model="state.techs" value='ajs'>:Angular
+    <input type="checkbox" v-model="state.techs" value='nxtjs'>:NextJS
+    <h2>{{state.techs}}</h2>
     <ul>
         <div v-for="(todo,i) in state.todos" :key="i">
             <Todo :todo="todo" :i="i" :toggle="toggle"></Todo>
         </div>
     </ul>
 </template>
+// <input type="radio" v-model="state.gender" value="m">:Male
+//     <input type="radio" v-model="state.gender" value="f">:Female
+//     <input type="radio" v-model="state.gender" value="o">:Others
+//     <h1>{{state.gender}}</h1>
